40d2ff72c087d859bda3832d4987d7749661d6b3
Bryan <limbryan12@gmail.com>
2023-06-17 21:06:27 +0800

    Test

---
+++ app/src/main/java/sg/edu/np/mad/madasgcacheflash/FlashCardQuestionPage.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.madasgcacheflash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FlashCardQuestionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_card_question_page);
+    }
+}
