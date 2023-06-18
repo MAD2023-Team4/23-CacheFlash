879d7860eee5673cb9f3657be314e7337b22bd2d
Black-catty <105922354+Black-catty@users.noreply.github.com>
2023-06-18 15:54:01 +0800

    John

--- app/src/main/java/sg/edu/np/mad/madasgcacheflash/FlashcardAdapter.java
+++ app/src/main/java/sg/edu/np/mad/madasgcacheflash/FlashcardAdapter.java
@@ -1,5 +1,8 @@
 package sg.edu.np.mad.madasgcacheflash;
 
+
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,20 +14,29 @@ import java.util.List;
 
 public class FlashcardAdapter extends RecyclerView.Adapter<FlashcardViewHolder> {
     private List<Flashcard> flashcardList;
+    private OnItemClickListener itemClickListener;
+
+    public interface OnItemClickListener {
+        void onItemClick(Flashcard flashcard);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        itemClickListener = listener;
+    }
 
     public FlashcardAdapter(List<Flashcard> flashcardList) {
         this.flashcardList = flashcardList;
     }
 
-    //@NonNull
-    //@Override
+    @NonNull
+    @Override
     public FlashcardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.flashcard_item, parent, false);
         return new FlashcardViewHolder(itemView);
     }
 
-    //@Override
+    @Override
     public void onBindViewHolder(@NonNull FlashcardViewHolder holder, int position) {
         Flashcard flashcard = flashcardList.get(position);
 
@@ -32,12 +44,22 @@ public class FlashcardAdapter extends RecyclerView.Adapter<FlashcardViewHolder> 
         holder.titleTextView.setText(flashcard.getTitle());
         holder.descTextView.setText(flashcard.getQuestions().size() + " questions");
         // Bind other views as needed
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Call the onItemClick method of the listener when the card is clicked
+                if (itemClickListener != null) {
+                    itemClickListener.onItemClick(flashcard);
+                    Log.v("Hi","Hi");
+                }
+            }
+        });
     }
 
-    //@Override
+    @Override
     public int getItemCount() {
         return flashcardList.size();
     }
-
-
 }
+
