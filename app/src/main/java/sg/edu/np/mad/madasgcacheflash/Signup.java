669421ada31efd28992cf184586fd8d8147d6f5f
Black-catty <105922354+Black-catty@users.noreply.github.com>
2023-06-25 23:11:17 +0800

    Changing Signup

--- app/src/main/java/sg/edu/np/mad/madasgcacheflash/Signup.java
+++ app/src/main/java/sg/edu/np/mad/madasgcacheflash/Signup.java
@@ -55,9 +55,11 @@ public class Signup extends AppCompatActivity {
                 */
 
                 User dbData = myDBHandler.findUser(etUsername.getText().toString());
-                if (dbData != null && dbData.getUsername().equals(etUsername.getText().toString())) {
+                if (dbData.getUsername().equals(etUsername.getText().toString())) {
                     Toast.makeText(Signup.this, "Username already exists!", Toast.LENGTH_SHORT).show();
                 }
+                else if(dbData != null)
+                {Toast.makeText(Signup.this, "Username already exists!", Toast.LENGTH_SHORT).show();}
                 else {
                     String dbUserName = etUsername.getText().toString();
                     String dbPassword = etPassword.getText().toString();
