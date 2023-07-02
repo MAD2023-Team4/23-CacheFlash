7d7848a18f307153443ca00482f218ad1e42dda1
DarkJoker521 <samuelkoh17@gmail.com>
2023-07-03 00:10:19 +0800

    Added a score feature first, so a score is calculated at the end of the quiz. Then, this score is the s value, or rather constant, of the forgetting curve. (very important)

--- app/src/main/java/sg/edu/np/mad/madasgcacheflash/Testyourself.java
+++ app/src/main/java/sg/edu/np/mad/madasgcacheflash/Testyourself.java
@@ -1,8 +1,10 @@
 package sg.edu.np.mad.madasgcacheflash;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,7 @@ import java.util.List;
 
 public class Testyourself extends AppCompatActivity {
 
+    int score = 0;
     final String TITLE = "Testyourself";
     int currentIndex = 0;
     List<String> questions = new ArrayList<>();
@@ -101,7 +104,12 @@ public class Testyourself extends AppCompatActivity {
             next.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    score++;
                     currentIndex++;
+                    if(currentIndex == flashcard.getQuestions().size()){
+                        //make a toast message of the score
+                        showAlert("Quiz Finished", "Your score: " + score, score, flashcard.getQuestions().size());
+                    }
                     if (currentIndex >= questions.size()) {
                         currentIndex = questions.size() - 1;
                     }
@@ -164,6 +172,27 @@ public class Testyourself extends AppCompatActivity {
     protected void onDestroy () {
         super.onDestroy();
         Log.v(TITLE, "On Destroy");
+    }
+
+    private void showAlert(String title, String text, int score, int total) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(text)
+                .setPositiveButton("View", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Do something when the "OK" button is clicked
+                        Intent intent = new Intent(Testyourself.this, MainActivity.class);
+                        intent.putExtra("Score", score);
+                        intent.putExtra("Total", total);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Do something when the "Cancel" button is clicked
+                    }
+                })
+                .show();
     }
 
 }
