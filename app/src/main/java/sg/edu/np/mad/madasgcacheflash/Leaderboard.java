85ee792b9c14967fe6dbf26f34457b918d71ad5a
DarkJoker521 <samuelkoh17@gmail.com>
2023-07-25 20:54:30 +0800

    ended up just not doing the leaderboard first (i commented out the code for leaderboard). we will settle it later.

--- app/src/main/java/sg/edu/np/mad/madasgcacheflash/Leaderboard.java
+++ app/src/main/java/sg/edu/np/mad/madasgcacheflash/Leaderboard.java
@@ -2,16 +2,29 @@ package sg.edu.np.mad.madasgcacheflash;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Leaderboard extends AppCompatActivity {
     private String username;
+    private ArrayList<User> userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,17 @@ public class Leaderboard extends AppCompatActivity {
         Intent intent = getIntent();
 
         username = intent.getStringExtra("Username"); //get username
+
+        /*
+        RecyclerView recyclerView = findViewById(R.id.searchRecyclerView);
+        cuAdapter = new SearchAdapter(flashcardList);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(cuAdapter);
+
+
+         */
         BottomNavigationView bottomNavigationView;
         bottomNavigationView = findViewById(R.id.bottom_navigator);
         bottomNavigationView.setSelectedItemId(R.id.leaderboard);
@@ -68,4 +92,31 @@ public class Leaderboard extends AppCompatActivity {
             }
         });
     }
+
+    //Method to query all the user data objects, plus the points
+    //___________________________________________________________
+    public void queryAllUsersAndPoints() {
+        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users");
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    // Retrieve the user object and its points attribute
+                    User user = userSnapshot.getValue(User.class);
+                    int points = user.getPoints();
+                    Log.v("User Points", "User: " + user.getUsername() + ", Points: " + points);
+                    userList.add(user);
+                    for (User u: userList){
+                        Log.v("user:",user.getUsername());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle any errors that occur
+            }
+        });
+    }
+
 }
